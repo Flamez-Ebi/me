18b9bac9340eb63c870cccd1bd9a9ef2f2f7d42a
Flamez-Ebi <93027120+Flamez-Ebi@users.noreply.github.com>
2023-06-18 23:04:47 +0100

    Update google-maps.js

--- google-maps.js
+++ google-maps.js
@@ -1,6 +1,7 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: 9.0479, lng: 7.5155};
+    var myLatLng = {lat: 7.5155, lng: 9.0479};
+
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
